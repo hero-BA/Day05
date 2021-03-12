15dd56d4bc2475629d8dd1a5a1c63a66d2fcc9a4
hero-BA <suad.hero@gmail.com>
2021-03-12 19:20:10 +0100

    Modifed exercise: Bracket notation v3

--- ex12/src/index.js
+++ ex12/src/index.js
@@ -1,6 +1,6 @@
 function main() {
     // Only change code below this line
-    var  lastName = "Tesla";
+    var lastName = "Tesla";
     var lastLetterOfLastName = lastName[lastName.length - 1];
     // Only change code above this line
     return lastLetterOfLastName;
