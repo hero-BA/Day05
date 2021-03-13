e8299238d3184b8893c52468ec9d9958078a34a4
hero-BA <suad.hero@gmail.com>
2021-03-13 10:33:09 +0100

    Modified exercise: Constructing strings with variables

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,6 +1,6 @@
 function main() {
     // Only change code below this line
-    var myName = "Suad Hero";
+    var myName = "Adriano";
     var myString = "My name is " + myName + " and I am well!";
     // Only change code above this line
     return myString;
