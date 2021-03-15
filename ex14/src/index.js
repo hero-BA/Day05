c833eab8656b4a83be3a659f309d0dc9b9c7a4dd
hero-BA <suad.hero@gmail.com>
2021-03-15 10:24:50 +0100

    Modifed exercise: Word blanks

--- ex14/src/index.js
+++ ex14/src/index.js
@@ -1,10 +1,10 @@
 function main() {
     // Only change code below this line
-    var myNoun = "dog ";
-    var myVerb = "ran ";
-    var myAdjective = "big ";
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
     var myAdverb = "quickly";
-    var wordBlanks = myNoun + myVerb + myAdjective + myAdverb + ".";
+    var wordBlanks = myNoun + " " + myVerb + " " + myAdjective + " " + myAdverb + ".";
     // Only change code above this line
     return wordBlanks;
 }
